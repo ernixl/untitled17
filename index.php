3b2b499685a625303f5899b12bdb42abf6c009c0
ernixl <ernixl@yahoo.com>
2017-02-28 15:39:12 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1615154
+ * Date: 28/02/2017
+ * Time: 15:38
+ */
+<! DOCTYPE ht ml >
+<ht ml >
+ <head>
+</ head>
+<body>
+ <p>
+ <?php
+ echo "Hell o Worl d";
+ ?>
+</ p>
+</ body>
+</ ht ml >
