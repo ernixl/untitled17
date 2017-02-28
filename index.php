f174a780bec1099c5498091e4e867e3dec4de247
ernixl <ernixl@yahoo.com>
2017-02-28 15:44:18 +0000

    fdshgewhw

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1615154
- * Date: 28/02/2017
- * Time: 15:38
- */
-<! DOCTYPE ht ml >
-<ht ml >
- <head>
-</ head>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
 <body>
- <p>
- <?php
- echo "Hell o Worl d";
- ?>
-</ p>
-</ body>
-</ ht ml >
+<p>
+    <?php
+    echo "Hello World";
+    ?>
+</p>
+</body>
+</html>
